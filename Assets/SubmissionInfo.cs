3d7b0cbb4d3d56f72f826aca89e0abf38716fc45
Takumi Takeshige <51091375+FSharpMajor@users.noreply.github.com>
2022-09-12 22:20:19 +1000

    updated blurb

--- Assets/SubmissionInfo.cs
+++ Assets/SubmissionInfo.cs
@@ -32,11 +32,9 @@ public static class SubmissionInfo
     // Write a brief blurb of your game, no more than 200 words. Again, ensure
     // this is final by the video milestone deadline.
     public static readonly string GameBlurb = 
-@"In this roguelike set in a futuristic cyber-fantasy dystopian Alabama, the player is the victim of twisted experiments conducted by
-<insert evil dude here>, leaving them with the ability to become enraged when on the brink of death. <--- are we going with the vampire cartel as the evil dude?
-Motivated by the need for revenge, the player must fight through hordes of enemies in the Alabama underground (or in the mine? We should decide on this), collecting items and power ups along the way.
-All enemies and rooms will be procedurally generated, with enemies being generated from a set of models and abilities <---- need to decide how we're procedurally generating rooms. We are going for dungeon style right?
-Since being experimented on, the player is losing their concept of humanity, and so to remind them of their humanity there is a familiar cat at each checkpoint that the player can interact with to heal";
+@"In this 2.5D top-down roguelike set in a futuristic cyber-fantasy dystopian Alabama, the player is the victim of twisted experiments, leaving them with the ability to become enraged when on the brink of death. 
+Motivated by the need for revenge, the player must fight through hordes of enemies in the Alabama underground, collecting items and power ups along the way. All enemies and rooms will be procedurally generated, with enemies being generated from a set of models and abilities. The monsters of the underground have been twisted by both corrupt organisations and the twisted environment.
+Since being experimented on, the player is slowly losing their concept of humanity, and so to remind them of their humanity there is a familiar cat at each checkpoint that the player can interact with to heal at discrete intervals throughout the game.";
     
     // By the gameplay video milestone deadline this should be a direct link
     // to a YouTube video upload containing your video. Ensure "Made for kids"
