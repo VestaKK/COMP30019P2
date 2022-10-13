26a2f40dc6fe5abb31b9e287ff9c854cccad361b
Expressionless <rhysmchaen@gmail.com>
2022-10-13 19:07:46 +1100

    updated Inventory

--- Assets/Code/Entities/Mobs/Player/Inventory/PlayerInventory.cs
+++ Assets/Code/Entities/Mobs/Player/Inventory/PlayerInventory.cs
@@ -6,7 +6,7 @@ public class PlayerInventory : MonoBehaviour
 {
     [SerializeField] private Image _uiSlot;
     private Slot<Weapon> _mainWeaponSlot;
-    private Slot<Weapon> _offHandSlot;
+    private Slot<Weapon> _offHandSlot; 
 
     void Awake() {
         _mainWeaponSlot = new Slot(this);
